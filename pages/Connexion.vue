98ad9ec9c8a279f37ae753cfdf70911b4e82a827
Gaël Leconte <gaelleconte@Gaels-MacBook-Pro.local>
2022-01-29 14:20:11 -0500

    V3 inscription/connexion

--- pages/Connexion.vue
+++ pages/Connexion.vue
@@ -1,54 +1,65 @@
 <template>
-  <v-row justify="center" align="center" style="height: 500px">
-    <v-col cols="5" sm="3" md="4">
-      <v-card color="secondary" style="paddingTop:30px">
-        <div class="d-flex flex-column justify-space-between align-center">
-          <v-col cols="5" sm="8">
-            <div>
-              <v-text-field
-                background-color="white"
-                label="Pseudo"
-                outlined
-                :rules="rules"
-                hide-details="auto"
-              ></v-text-field>
-            </div>
-            <div style='paddingTop:4px'>
-              <v-text-field
-                background-color="white"
-                outlined
-                v-model="password"
-                :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-                :rules="[rules.required, rules.min]"
-                :type="show1 ? 'text' : 'password'"
-                name="input-10-1"
-                label="Mot de passe"
-                hint="Au moins 8 characters"
-                counter
-                @click:append="show1 = !show1"
-              ></v-text-field>
-            </div>
-          </v-col>
-          <v-col cols="5" sm="5">
-            <v-btn color="primary" elevation="2" block> Connexion </v-btn>
-          </v-col>
+  <v-container fluid class="pa-0">
+    <v-img
+      v-bind="props"
+      class="ml"
+      :src="require('./../static/Background.jpeg')"
+      max-height="60%"
+      max-width="100%"
+      :height="bgHeight"
+    >
+      <v-row justify="center" align="center" style="height: 600px">
+        <v-col cols="5" sm="3" md="4">
+          <v-card color="secondary" style="padding: 10px">
+            <div class="d-flex flex-column justify-space-between align-center">
+              <v-col cols="5" sm="8">
+                <div style="padding: 2px">
+                  <v-text-field
+                    background-color="white"
+                    label="Pseudo"
+                    outlined
+                    :rules="rules"
+                    hide-details="auto"
+                  ></v-text-field>
+                </div>
+                <div style="padding: 2px">
+                  <v-text-field
+                    background-color="white"
+                    outlined
+                    v-model="password"
+                    :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                    :rules="[rules.required, rules.min]"
+                    :type="show1 ? 'text' : 'password'"
+                    name="input-10-1"
+                    label="Mot de passe"
+                    hint="Au moins 8 characters"
+                    counter
+                    @click:append="show1 = !show1"
+                  ></v-text-field>
+                </div>
+              </v-col>
+              <v-col cols="2" sm="5">
+                <v-btn color="primary" elevation="2" block> Connexion </v-btn>
+              </v-col>
 
-          <v-col cols="5" sm="5">
-            <v-btn color="primary" elevation="2" block> Inscription </v-btn>
-          </v-col>
-        </div>
-        <v-card-text
-          class="d-flex flex-column justify-space-between align-center"
-        >
-        </v-card-text>
-      </v-card>
-    </v-col>
-  </v-row>
+              <v-col cols="2" sm="5">
+                <v-btn color="primary" elevation="2" block> Inscription </v-btn>
+              </v-col>
+            </div>
+            <v-card-text
+              class="d-flex flex-column justify-space-between align-center"
+            >
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-container>
 </template>
 
 <script>
 export default {
-   data() {
+  data() {
     return {
       show1: false,
       password: '',
